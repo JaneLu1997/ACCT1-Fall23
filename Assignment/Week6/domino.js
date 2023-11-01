67de3fd3cced60404d28a72817988ae968d1a089
JaneLu1997 <janelu.7.design@gmail.com>
2023-11-01 16:08:16 -0700

    based code for the dominos

--- Assignment/Week6/domino.js
+++ Assignment/Week6/domino.js
@@ -1,15 +1,20 @@
-class Domino{
-    constructor(x1, y1, x2, y2){
-        this.position = createVector(x1, y1, x2, y2)
-        this.trickle = createVector(0,0)
-        this.cColor = (255)
+class Domino {
+    constructor(x1, y1, x2, y2) {
+        this.position = {
+            x1: x1,
+            y1: y1,
+            x2: x2,
+            y2: y2,
+        };
+        this.cColor = 255;
     }
 
-    display(){
+    display() {
         stroke(this.cColor)
         noFill()
-        line(this.position.x1, this.position.y1, this.position.x2, this.position.y2)
-
-    
+				strokeWeight(4)
+        for (let x1 = this.position.x1; x1 < windowWidth - 80; x1 += 60) {
+            line(x1, this.position.y1, x1, this.position.y2);
+        }
     }
 }
--- Assignment/Week6/system_sketch.js
+++ Assignment/Week6/system_sketch.js
@@ -1,16 +1,21 @@
-let domino =[]
-let force
-let tiltAng
-let gravity
+let dominos =[]
 
-function setup(){
-    createCanvas(windowWidth, windowHeight)
-    colorMode(HSB, TWO_PI, 1, 1)
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    colorMode(HSB, TWO_PI, 1, 1);
+		background(0)
 
-    force = rotate()
+    // Create multiple instances of Domino with different x1 positions
+    const dominos = [];
+    const y1 = 100;
+    const y2 = 200;
 
+    for (let x1 = 100; x1 < windowWidth - 80; x1 += 60) {
+        dominos.push(new Domino(x1, y1, x1 , y2));
+    }
+
+    dominos.forEach((domino) => {
+        domino.display();
+    });
 }
 
-function draw(){
-    background(0)
-}
